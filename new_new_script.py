0199f79738da6da39d37cd509f591aa3053a7d2c
Craleks <66841597+Craleks@users.noreply.github.com>
2024-05-17 02:57:20 +0700

    17

--- new_new_script.py
+++ new_new_script.py
@@ -1,36 +1,62 @@
 import pyodbc as db
 import random
 from data import *
-from mimesis import Address
+from mimesis import Address, Person
 from mimesis.enums import Gender
 from mimesis.builtins import RussiaSpecProvider
 
 address = Address("ru")
+person = Person("ru")
+ru = RussiaSpecProvider()
 
 table_filling_order = [
     "Addresses",
-    # "Specializations",
-    # "Doctors",
-    # "Patients",
+    "Specializations",
+    "Doctors",
+    "Patients",
     # "Diseases",
     # "Appointments",
     # "FactTable",
 ]
 
-def generate_data():
-    street_name = "ул. " + address.street_name()
-    house_number = str(random.randint(1, 200))
-    apartment_number = "кв. " + str(random.randint(1, 200))
-    full_address = f"{street_name} {house_number}, {apartment_number}"
 
+def get_specialization_ids(cursor):
+    cursor.execute("SELECT SpecializationID FROM Specializations")
+    return [row.SpecializationID for row in cursor.fetchall()]
+
+
+def get_address_ids(cursor):
+    cursor.execute("SELECT AddressID FROM Addresses")
+    return [row.AddressID for row in cursor.fetchall()]
+
+
+def generate_data():
+    rd_gender = random.choice([Gender.MALE, Gender.FEMALE])
+    specialization_ids = get_specialization_ids(cursor)
+    address_ids = get_address_ids(cursor)
     data = {
         "Addresses": {
-            "Street": full_address,
+            "Street": f"ул. {address.street_name()} {random.randint(1, 200)}, кв. {random.randint(1, 200)}",
             "City": address.city(),
             "Districts": random.choice(districts),
         },
+        "Specializations": {
+            "SpecializationName": random.choice(names_specializations),
+        },
+        "Doctors": {
+            "SpecializationID": random.choice(specialization_ids),
+            "FIO": f"{person.last_name(gender=rd_gender)} {person.first_name(gender=rd_gender)} {ru.patronymic(gender=rd_gender)}",
+            "PhoneNumber": person.phone_number(),
+        },
+        "Patients": {
+            "AddressID": random.choice(address_ids),
+            "FIO": f"{person.last_name(gender=rd_gender)} {person.first_name(gender=rd_gender)} {ru.patronymic(gender=rd_gender)}",
+            "DateOfBirth": person.birthdate(min_year=1980, max_year=2023),
+            "Gender": str(rd_gender.name)[0],
+        },
     }
     return data
+
 
 def request_execution(cursor, request):
     try:
@@ -40,10 +66,14 @@ def request_execution(cursor, request):
     except Exception as e:
         print(f"Произошла ошибка: {e}")
 
+
 def get_primary_keys(cursor, table_name):
-    cursor.execute(f"SELECT column_name FROM information_schema.key_column_usage WHERE OBJECTPROPERTY(OBJECT_ID(constraint_name), 'IsPrimaryKey') = 1 AND table_name = '{table_name}'")
+    cursor.execute(
+        f"SELECT column_name FROM information_schema.key_column_usage WHERE OBJECTPROPERTY(OBJECT_ID(constraint_name), 'IsPrimaryKey') = 1 AND table_name = '{table_name}'"
+    )
     primary_keys = [row.column_name for row in cursor.fetchall()]
     return primary_keys
+
 
 def getting_table_names(cursor):
     cursor.execute(
@@ -51,6 +81,7 @@ def getting_table_names(cursor):
     )
     tables = [row.table_name for row in cursor.fetchall()]
     return tables
+
 
 def getting_column_name(tables):
     columns = {}
@@ -60,8 +91,11 @@ def getting_column_name(tables):
         )
         columns[table] = [row.column_name for row in cursor.fetchall()]
         primary_keys = get_primary_keys(cursor, table)
-        columns[table] = [column for column in columns[table] if column not in primary_keys]
+        columns[table] = [
+            column for column in columns[table] if column not in primary_keys
+        ]
     return columns
+
 
 def adding_data(columns, table_name, num_records):
     if table_name in columns:
@@ -74,7 +108,8 @@ def adding_data(columns, table_name, num_records):
             values = ", ".join(f"'{value}'" for value in data.values())
 
             print(f"INSERT INTO {table_name} ({clear_column_names}) VALUES ({values});")
-            return f"INSERT INTO {table_name} ({clear_column_names}) VALUES ({values});"
+            # return f"INSERT INTO {table_name} ({clear_column_names}) VALUES ({values});"
+
 
 connection = db.connect(
     r"driver={ODBC Driver 17 for SQL Server}; server=DESKTOP-C8OR9VL\SQLEXPRESS; database=main_hospital; trusted_connection=yes"
